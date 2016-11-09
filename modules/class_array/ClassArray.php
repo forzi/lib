6c06b3839e54f7dbd3fc7b5e520d3aeda7167ed6
forziforzi@gmail.com <forziforzi@gmail.com>
2016-11-09 21:59:44 +0200

    EventClassArray

--- modules/class_array/ClassArray.php
+++ modules/class_array/ClassArray.php
@@ -2,17 +2,21 @@
 namespace stradivari\class_array;
 
 class ClassArray implements \ArrayAccess, \Iterator {
-    protected $storage = array();
-
-    public function __construct($data = null) {
-        self::init($data);
+    protected $storage = [];
+	
+	protected function createSubclass($data) {
+		return new static($data);
+	}
+	
+    public function __construct($data = []) {
+		$this->init($data);
     }
     public function init($data = []) {
         $this->storage = [];
-        if (is_array($data) || $data instanceof \Traversable) {
+        if ($data && (is_array($data) || $data instanceof \Traversable)) {
             foreach ($data as $key => &$subData) {
                 if (is_array($subData) || $subData instanceof \Traversable) {
-                    $subData = new ClassArray($subData);
+                    $subData = $this->createSubclass($subData);
                 }
                 $this->storage[$key] = $subData;
             }
@@ -38,7 +42,7 @@ class ClassArray implements \ArrayAccess, \Iterator {
         return true;
     }
     public function offsetGet($key) {
-        if ( !isset($this->storage[$key]) ) {
+        if (!isset($this->storage[$key])) {
             if ( strpos($key, '__list') === false ) {
                 return null;
             } else {
@@ -51,27 +55,31 @@ class ClassArray implements \ArrayAccess, \Iterator {
         $key = $key === null ? count($this->storage) : $key;
         if ($value === null) {
             $this->offsetUnset($key);
-        } else if ($value instanceof ClassArray) {
-            $this->storage[$key] = clone $value;
-        } else if (is_array($value) || $value instanceof \Traversable) {
-            $this->storage[$key] = new ClassArray($value);
+			return;
+        }
+		if (is_array($value) || $value instanceof \Traversable) {			
+            $this->storage[$key] = $this->createSubclass($value);
         } else {
             $this->storage[$key] = $value;
         }
     }
     public function offsetUnset($key) {
-        if ( isset($this->storage[$key]) ) {
-            unset($this->storage[$key]);
-        }
+        if (!isset($this->storage[$key])) {
+			return;
+		}
+		unset($this->storage[$key]);
     }
     public function toArray() {
         $array = [];
         foreach ($this->storage as $key => $val) {
-            if ($val instanceof self) {
+            if ($val instanceof static) {
                 $val = $val->toArray();
             }
             $array[$key] = $val;
         }
         return $array;
     }
+	public function __get($name) {
+		return $this->$name;
+	}
 }
---
+++ modules/class_array/EventClassArray.php
@@ -0,0 +1,59 @@
+<?
+namespace stradivari\class_array;
+
+class EventClassArray extends ClassArray {
+	protected $parent;
+	protected $beforeSet;
+	protected $onSet;
+	protected $beforeUnset;
+	protected $onUnset;
+
+	protected function createSubclass($data) {
+		return new static($data, $this, $this->beforeSet, $this->onSet, $this->beforeUnset, $this->onUnset);
+	}
+	
+    public function __construct(
+		$data = [], 
+		self $parent = null, 
+		callable $beforeSet = null, 
+		callable $onSet = null, 
+		callable $beforeUnset = null, 
+		callable $onUnset = null
+	) {
+		$this->beforeSet = $beforeSet;
+        $this->onSet = $onSet;
+		$this->beforeUnset = $beforeUnset;
+		$this->onUnset = $onUnset;
+		$this->init($data);
+    }
+    public function offsetSet($key, $value) {
+        $key = $key === null ? count($this->storage) : $key;
+        if ($value === null) {
+            $this->offsetUnset($key);
+			return;
+        } 
+		if (is_callable($this->beforeSet)) {
+		    $this->beforeSet($this, $key, $value);
+		}
+		if (is_array($value) || $value instanceof \Traversable) {			
+            $this->storage[$key] = $this->createSubclass($value);
+        } else {
+            $this->storage[$key] = $value;
+        }
+		if (is_callable($this->onSet)) {
+		    $this->onSet($this, $key, $value);
+		}
+    }
+    public function offsetUnset($key) {
+        if (!isset($this->storage[$key])) {
+			return;
+		}
+		if (is_callable($this->beforeUnset)) {
+			$this->beforeUnset($this, $key);
+		}
+		unset($this->storage[$key]);
+		if (is_callable($this->onUnset)) {
+			$this->onUnset($this, $key);
+		}
+    }
+}
